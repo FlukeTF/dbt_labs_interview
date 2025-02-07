02436b972529be943189695396df2cad23494163
Ryan Luke Daque <rlmdaque@gmail.com>
2025-02-07 23:15:41 +0000

    adding sources and updating staging models

--- models/staging/dbt_labs_interview/stg_interview_task__orders.sql
+++ models/staging/dbt_labs_interview/stg_interview_task__orders.sql
@@ -1,11 +1,25 @@
-select 
-    cast(order_id as string) as order_id,
-    cast(user_id as string) as user_id,
-    created_at,
-    updated_at,
-    shipped_at,
-    currency,
-    status,
-    amount_total_cents,
-    shipping_method
-from `dbt-public.interview_task.orders`
+with 
+
+source as (
+
+    select * from {{ source('interview_task', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        cast(order_id as string) as order_id,
+        cast(user_id as string) as user_id,
+        created_at,
+        updated_at,
+        shipped_at,
+        currency,
+        status,
+        amount_total_cents,
+        shipping_method
+    from source
+
+)
+
+select * from renamed
--- models/staging/dbt_labs_interview/stg_interview_task__payments.sql
+++ models/staging/dbt_labs_interview/stg_interview_task__payments.sql
@@ -1,10 +1,25 @@
-select
-    cast(order_id as string) as order_id,
-    cast(payment_id as string) as payment_id,
-    status,
-    payment_type,
-    amount_cents,
-    tax_amount_cents,
-    amount_shipping_cents,    
-    created_at
-from `dbt-public.interview_task.payments`
+with 
+
+source as (
+
+    select * from {{ source('interview_task', 'payments') }}
+
+),
+
+renamed as (
+
+    select
+        cast(order_id as string) as order_id,
+        cast(payment_id as string) as payment_id,
+        status,
+        payment_type,
+        amount_cents,
+        tax_amount_cents,
+        amount_shipping_cents,    
+        created_at
+
+    from source
+
+)
+
+select * from renamed
